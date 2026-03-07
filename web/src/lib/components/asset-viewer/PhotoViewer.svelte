1d9308eb404762a74d2c11e73e512bce164b2b5e
midzelis <min123@gmail.com>
2026-03-07 21:14:45 +0000

    fix(web): preserve stacked asset selection when tagging faces
    Change-Id: Iec1507560f99f2e9433bd5cf6b460b176a6a6964

--- web/src/lib/components/asset-viewer/PhotoViewer.svelte
+++ web/src/lib/components/asset-viewer/PhotoViewer.svelte
@@ -30,9 +30,10 @@
     onReady?: () => void;
     onError?: () => void;
     onSwipe?: (event: SwipeCustomEvent) => void;
+    onTagFace?: () => Promise<void>;
   };
 
-  let { cursor, element = $bindable(), sharedLink, onReady, onError, onSwipe }: Props = $props();
+  let { cursor, element = $bindable(), sharedLink, onReady, onError, onSwipe, onTagFace }: Props = $props();
 
   const { slideshowState, slideshowLook } = slideshowStore;
   const asset = $derived(cursor.current);
@@ -285,6 +286,12 @@
   </AdaptiveImage>
 
   {#if assetViewerManager.isFaceEditMode && assetViewerManager.imgRef}
-    <FaceEditor htmlElement={assetViewerManager.imgRef} {containerWidth} {containerHeight} assetId={asset.id} />
+    <FaceEditor
+      htmlElement={assetViewerManager.imgRef}
+      {containerWidth}
+      {containerHeight}
+      assetId={asset.id}
+      {onTagFace}
+    />
   {/if}
 </div>
